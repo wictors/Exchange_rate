29079c4c4b62b020223ed98c3f0003bab59fedca
wictors <viktors5896@gmail.com>
2025-02-24 08:02:01 +0100

    fix restart containers

--- src/external/externalApiService.ts
+++ src/external/externalApiService.ts
@@ -1,8 +1,7 @@
 import axios from 'axios';
 
 const API_KEY = process.env.EXCHANGE_RATE_API_KEY;
-const BASE_URL =
-  process.env.EXTERNAL_API_BASE_URL;
+const BASE_URL = process.env.EXTERNAL_API_BASE_URL;
 
 export async function fetchCurrentRates(code: string): Promise<any> {
   try {
@@ -22,7 +21,6 @@ export async function fetchHistoricalRates(
   day: number,
 ): Promise<any> {
   try {
-    
     const url = `${BASE_URL}/${API_KEY}/history/${code}/${year}/${month}/${day}`;
     const response = await axios.get(url);
     return response.data;
